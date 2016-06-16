1015451e3cee61bf29728607cb208de1d67facf6
mswift42 <martin.haesler@gmail.com>
2016-06-16 19:11:26 +0100

    add class Recipe

---
+++ src/app/recipe.service.spec.ts
@@ -0,0 +1,14 @@
+/* tslint:disable:no-unused-variable */
+
+import {
+  beforeEach, beforeEachProviders,
+  describe, xdescribe,
+  expect, it, xit,
+  async, inject
+} from '@angular/core/testing';
+import { Recipe} from './recipe.service';
+
+describe('Recipe Service', () => {
+  beforeEachProviders(() => [Recipe]);
+
+});
---
+++ src/app/recipe.service.ts
@@ -0,0 +1,8 @@
+
+export class Recipe {
+
+  constructor(public publisher: String, public title: String,
+    public source_url: String, public image_url: String,
+    public publisher_url: String) { }
+
+}
