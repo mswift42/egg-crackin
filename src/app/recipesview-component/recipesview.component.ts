a33efbfe6ad613d881818ddc1c720c09d3594d47
mswift42 <martin.haesler@gmail.com>
2016-06-18 12:50:38 +0100

    Add md_input directives

--- src/app/recipesview-component/recipesview.component.ts
+++ src/app/recipesview-component/recipesview.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import {MdToolbar} from '@angular2-material/toolbar';
+import {MD_INPUT_DIRECTIVES} from '@angular2-material/input';
 
 @Component({
   moduleId: module.id,
   selector: 'app-recipesview',
   templateUrl: 'recipesview.component.html',
   styleUrls: ['recipesview.component.css'],
-  directives: [MdToolbar]
+  directives: [MdToolbar, MD_INPUT_DIRECTIVES]
 })
 export class RecipesviewComponent{
 
