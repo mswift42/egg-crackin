30ab393d3747c64e13fb5626180a868b319e6167
mswift42 <martin.haesler@gmail.com>
2016-06-18 18:53:22 +0100

    Generate recipe_url component

---
+++ src/app/recipe-url/recipe-url.component.spec.ts
@@ -0,0 +1,20 @@
+/* tslint:disable:no-unused-variable */
+
+import { By }           from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import {
+  beforeEach, beforeEachProviders,
+  describe, xdescribe,
+  expect, it, xit,
+  async, inject
+} from '@angular/core/testing';
+
+import { RecipeUrlComponent } from './recipe-url.component';
+
+describe('Component: RecipeUrl', () => {
+  it('should create an instance', () => {
+    let component = new RecipeUrlComponent();
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/recipe-url/recipe-url.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-recipe-url',
+  templateUrl: 'recipe-url.component.html',
+  styleUrls: ['recipe-url.component.css']
+})
+export class RecipeUrlComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {
+  }
+
+}
