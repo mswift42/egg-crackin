9efe4607ed6ccf4b5b3830479b04407e88416127
mswift42 <martin.haesler@gmail.com>
2016-07-02 09:34:40 +0100

    Generate title component

---
+++ src/app/title/title.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-title',
+  templateUrl: 'title.component.html',
+  styleUrls: ['title.component.css']
+})
+export class TitleComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {
+  }
+
+}
--- src/system-config.ts
+++ src/system-config.ts
@@ -53,6 +53,7 @@ const barrels: string[] = [
   'app/recipe-image',
   'app/publisher-url',
   'app/favourite',
+  'app/title',
   /** @cli-barrel */
 ];
 
