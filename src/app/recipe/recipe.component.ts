98237bab01d934ed566a1899493f821f770ab00e
mswift42 <martin.haesler@gmail.com>
2016-07-02 08:25:51 +0100

    Import Recipe image,url publisher components

--- src/app/recipe/recipe.component.ts
+++ src/app/recipe/recipe.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import {RecipeImageComponent} from '../recipe-image/recipe-image.component';
+import {RecipeUrlComponent} from '../recipe-url/recipe-url.component';
+import {PublisherUrlComponent} from '../publisher-url/publisher-url.component';
 
 @Component({
   moduleId: module.id,
   selector: 'app-recipe',
   templateUrl: 'recipe.component.html',
-  styleUrls: ['recipe.component.css']
+  styleUrls: ['recipe.component.css'],
+  directives: [RecipeImageComponent, PublisherUrlComponent, RecipeUrlComponent]
 })
 export class RecipeComponent {
 
