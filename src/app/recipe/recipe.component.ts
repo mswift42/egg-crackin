67490706527568d0e2504ca004af8b3ce45250d2
mswift42 <martin.haesler@gmail.com>
2016-07-02 09:36:02 +0100

    Replace recipeurl with title component

--- src/app/recipe/recipe.component.ts
+++ src/app/recipe/recipe.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {RecipeImageComponent} from '../recipe-image/recipe-image.component';
-import {RecipeUrlComponent} from '../recipe-url/recipe-url.component';
 import {PublisherUrlComponent} from '../publisher-url/publisher-url.component';
 import {FavouriteComponent} from '../favourite/favourite.component';
+import {TitleComponent} from '../title/title.component';
 
 @Component({
   moduleId: module.id,
@@ -10,7 +10,7 @@ import {FavouriteComponent} from '../favourite/favourite.component';
   templateUrl: 'recipe.component.html',
   styleUrls: ['recipe.component.css'],
   directives: [RecipeImageComponent, PublisherUrlComponent,
-    RecipeUrlComponent, FavouriteComponent]
+    TitleComponent, FavouriteComponent]
 })
 export class RecipeComponent {
 
