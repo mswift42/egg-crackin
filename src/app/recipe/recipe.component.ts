9a5e915a28414718b6817c299f722a01270c649c
mswift42 <martin.haesler@gmail.com>
2016-07-02 08:28:16 +0100

    Import favourite component

--- src/app/recipe/recipe.component.ts
+++ src/app/recipe/recipe.component.ts
@@ -2,17 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import {RecipeImageComponent} from '../recipe-image/recipe-image.component';
 import {RecipeUrlComponent} from '../recipe-url/recipe-url.component';
 import {PublisherUrlComponent} from '../publisher-url/publisher-url.component';
+import {FavouriteComponent} from '../favourite/favourite.component';
 
 @Component({
   moduleId: module.id,
   selector: 'app-recipe',
   templateUrl: 'recipe.component.html',
   styleUrls: ['recipe.component.css'],
-  directives: [RecipeImageComponent, PublisherUrlComponent, RecipeUrlComponent]
+  directives: [RecipeImageComponent, PublisherUrlComponent,
+    RecipeUrlComponent, FavouriteComponent]
 })
 export class RecipeComponent {
 
-  constructor() {}
+  constructor() { }
 
 
 }
