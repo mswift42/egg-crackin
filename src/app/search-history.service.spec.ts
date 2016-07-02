5dab7a48619eea5c7864b1069dba5ed9821f56e9
mswift42 <martin.haesler@gmail.com>
2016-07-02 14:09:01 +0100

    Add check for saveSearch method

--- src/app/search-history.service.spec.ts
+++ src/app/search-history.service.spec.ts
@@ -24,3 +24,26 @@ describe('Searchhistory getter ', () => {
   inject([SearchHistoryService], (service: SearchHistoryService) =>
   expect(service.searchhistory.length).toBe(0)));
 });
+
+describe('saveSearch method ', () => {
+  beforeEachProviders(() => [SearchHistoryService]);
+
+  it('should add to searchhistory array',
+  inject([SearchHistoryService], (service: SearchHistoryService) => {
+    let q1 = "ham";
+    let q2 = "butter";
+    service.saveSearch(q1);
+    service.saveSearch(q2);
+    expect(service.searchhistory.length).toBe(2);
+  }));
+  it('should store in localStorage',
+  inject([SearchHistoryService], (service: SearchHistoryService) => {
+    let q1 = "ham";
+    service.saveSearch(q1);
+    expect(service.searchhistory[0]).toBe("ham");
+    service.loadFromStorage();
+    expect(service.searchhistory[0]).toBe("ham"); 
+  }))
+
+
+});
