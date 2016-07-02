e07b51273c2db532bdde0d2f8068b0123c83469b
mswift42 <martin.haesler@gmail.com>
2016-07-02 08:55:41 +0100

    add saveToStorage method

--- src/app/search-history.service.ts
+++ src/app/search-history.service.ts
@@ -2,7 +2,33 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class SearchHistoryService {
+  private _searchhistory = [];
+  private _storagename = 'SEARCHHISTORY';
+  private _localStorage = window.localStorage;
 
   constructor() {}
+
+  contains(search: string): boolean {
+    return this._searchhistory.filter((i) => (i === search)).length > 0;
+  }
+
+  saveSearch(search: string) {
+    if (!(this.contains(search))) {
+      this._searchhistory.unshift(search);
+      this.saveToStorage();
+      if (this._searchhistory.length > 10) {
+        this._searchhistory.pop();
+        this.saveToStorage();
+      }
+    }
+  }
+
+  saveToStorage() {
+    this._localStorage.setItem(this._storagename, this._storagename);
+  }
+
+  loadFromStorage() {
+    this._searchhistory = this._localStorage.getItem(this._storagename);
+  }
 
 }
