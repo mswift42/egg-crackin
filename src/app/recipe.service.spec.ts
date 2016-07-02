3457c5d8c97ef6a97a01ae45b203b2ee13eebe98
mswift42 <martin.haesler@gmail.com>
2016-07-02 10:52:50 +0100

    add multiple checks for queryUrl

--- src/app/recipe.service.spec.ts
+++ src/app/recipe.service.spec.ts
@@ -21,5 +21,26 @@ describe('Recipe Service', () => {
     inject([RecipeService], (service: RecipeService) => {
       let key = service.food2forkKey;
       expect(key).toBe("7987c43afcf8a03a964bbcb0c9152c84");
-    }))
+    }));
+});
+
+describe('Query Url', () => {
+  beforeEachProviders(() => [RecipeService, Http, ConnectionBackend,
+  HTTP_PROVIDERS]);
+
+  it('should return base url with an empty string as input',
+  inject([RecipeService], (service: RecipeService) => {
+    let url = service.queryUrl("");
+    expect(url).toBe("http://food2fork.com/api/search?key=7987c43afcf8a03a964bbcb0c9152c84&q=");
+  }));
+  it('should append a single query parameter',
+  inject([RecipeService], (service: RecipeService) => {
+    let url = service.queryUrl("ham");
+    expect(url).toBe("http://food2fork.com/api/search?key=7987c43afcf8a03a964bbcb0c9152c84&q=ham")
+  }));
+  it('should append multiple parametrs seperated by &',
+  inject([RecipeService], (service: RecipeService) => {
+    let url = service.queryUrl("ham butter");
+    expect(url).toBe("http://food2fork.com/api/search?key=7987c43afcf8a03a964bbcb0c9152c84&q=ham&butter");
+  }));
 });
