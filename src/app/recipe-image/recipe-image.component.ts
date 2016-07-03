ba9416b304d2c05870f3fa67fcc033b9a1274896
mswift42 <martin.haesler@gmail.com>
2016-07-03 09:24:57 +0100

    Import md-card directives.

--- src/app/recipe-image/recipe-image.component.ts
+++ src/app/recipe-image/recipe-image.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit, Input } from '@angular/core';
+import {MD_CARD_DIRECTIVES} from '@angular2-material/card';
 
 @Component({
   moduleId: module.id,
   selector: 'app-recipe-image',
   templateUrl: 'recipe-image.component.html',
-  styleUrls: ['recipe-image.component.css']
+  styleUrls: ['recipe-image.component.css'],
+  directives: [MD_CARD_DIRECTIVES]
 })
 export class RecipeImageComponent implements OnInit {
 
