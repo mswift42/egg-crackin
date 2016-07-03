78b0074a41115c016683bf0d4ea1b05b4a12d311
mswift42 <martin.haesler@gmail.com>
2016-07-03 08:01:40 +0100

    add recipe inputs

--- src/app/recipe/recipe.component.ts
+++ src/app/recipe/recipe.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input} from '@angular/core';
 import {RecipeImageComponent} from '../recipe-image/recipe-image.component';
 import {PublisherUrlComponent} from '../publisher-url/publisher-url.component';
 import {FavouriteComponent} from '../favourite/favourite.component';
 import {TitleComponent} from '../title/title.component';
+import {MD_CARD_DIRECTIVES} from '@angular2-material/card';
 
 @Component({
   moduleId: module.id,
@@ -10,9 +11,14 @@ import {TitleComponent} from '../title/title.component';
   templateUrl: 'recipe.component.html',
   styleUrls: ['recipe.component.css'],
   directives: [RecipeImageComponent, PublisherUrlComponent,
-    TitleComponent, FavouriteComponent]
+    TitleComponent, FavouriteComponent, MD_CARD_DIRECTIVES]
 })
 export class RecipeComponent {
+  @Input() title: string;
+  @Input() recipe_url: string;
+  @Input() image_url: string;
+  @Input() publisher: string;
+  @Input() publisher_url: string;
 
   constructor() { }
 
