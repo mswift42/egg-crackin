df1da2cf999d7ba628cbc3088feb1f3cd9e72a15
mswift42 <martin.haesler@gmail.com>
2016-07-03 20:27:56 +0100

    Implement keyup.enter handler

--- src/app/recipesview-component/recipesview.component.ts
+++ src/app/recipesview-component/recipesview.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {MdToolbar} from '@angular2-material/toolbar';
 import {MD_INPUT_DIRECTIVES} from '@angular2-material/input';
+import {Recipe, RecipeService} from '../recipe.service';
 
 @Component({
   moduleId: module.id,
@@ -9,9 +10,15 @@ import {MD_INPUT_DIRECTIVES} from '@angular2-material/input';
   styleUrls: ['recipesview.component.css'],
   directives: [MdToolbar, MD_INPUT_DIRECTIVES]
 })
-export class RecipesviewComponent{
+export class RecipesviewComponent {
 
-  constructor() {}
+  recipes: Recipe[];
 
+  constructor(private recipeservice: RecipeService) { }
+
+  searchRecipes(query: string) {
+    this.recipeservice.getRecipes(query).then(recipes => 
+    this.recipes = recipes);
+  }
 
 }
