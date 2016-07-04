0d27ed4c7cad32e0c65245b82f1a97a709940e58
mswift42 <martin.haesler@gmail.com>
2016-07-04 17:38:18 +0100

    Add markup and inputs

--- src/app/publisher-url/publisher-url.component.ts
+++ src/app/publisher-url/publisher-url.component.ts
@@ -1,16 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  moduleId: module.id,
-  selector: 'app-publisher-url',
-  templateUrl: 'publisher-url.component.html',
-  styleUrls: ['publisher-url.component.css']
+    moduleId: module.id,
+    selector: 'app-publisher-url',
+    templateUrl: 'publisher-url.component.html',
+    styleUrls: ['publisher-url.component.css']
 })
-export class PublisherUrlComponent implements OnInit {
+export class PublisherUrlComponent {
+    @Input() publisher: string;
+    @Input() publisher_url: string;
 
-  constructor() {}
+    constructor() {}
 
-  ngOnInit() {
-  }
 
 }
