4c05a0b48dcca9b92333349702d500b364551e7f
mswift42 <martin.haesler@gmail.com>
2016-07-04 20:15:14 +0100

    Delete onInit

--- src/app/title/title.component.ts
+++ src/app/title/title.component.ts
@@ -1,19 +1,17 @@
-import { Component, OnInit, Input} from '@angular/core';
+import { Component, Input} from '@angular/core';
 
 @Component({
-  moduleId: module.id,
-  selector: 'app-title',
-  templateUrl: 'title.component.html',
-  styleUrls: ['title.component.css']
+    moduleId: module.id,
+    selector: 'app-title',
+    templateUrl: 'title.component.html',
+    styleUrls: ['title.component.css']
 })
-export class TitleComponent implements OnInit {
+export class TitleComponent {
 
-  @Input() title: string;
-  @Input() recipe_url: string;
+    @Input() title: string;
+    @Input() recipe_url: string;
 
-  constructor() {}
+    constructor() {}
 
-  ngOnInit() {
-  }
 
 }
