8f7185720fd92c469b554c22d1f18104c110c523
mswift42 <martin.haesler@gmail.com>
2016-07-04 17:22:52 +0100

    Add recipeservice to providers.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {RecipesviewComponent} from './recipesview-component/recipesview.component';
+import {RecipeService} from './recipe.service';
 
 @Component({
-  moduleId: module.id,
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css'],
-  directives: [RecipesviewComponent]
+    moduleId: module.id,
+    selector: 'app-root',
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.component.css'],
+    directives: [RecipesviewComponent],
+    providers: [RecipeService]
 })
 export class AppComponent {
 }
