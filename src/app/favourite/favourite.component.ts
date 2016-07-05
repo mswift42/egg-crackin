03aa223e14a8200610301ca8a135df039bd03673
mswift42 <martin.haesler@gmail.com>
2016-07-05 17:37:00 +0100

    Display and import tooltip

--- src/app/favourite/favourite.component.ts
+++ src/app/favourite/favourite.component.ts
@@ -1,10 +1,12 @@
 import { Component,Input } from '@angular/core';
+import {TooltipComponent} from '../tooltip/tooltip.component';
 
 @Component({
   moduleId: module.id,
   selector: 'app-favourite',
   templateUrl: 'favourite.component.html',
-  styleUrls: ['favourite.component.css']
+  styleUrls: ['favourite.component.css'],
+  directives: [TooltipComponent]
 })
 export class FavouriteComponent {
 
