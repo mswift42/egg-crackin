66d200230e7cb789ad468357c9ba4ccf622679a7
mswift42 <martin.haesler@gmail.com>
2016-07-06 17:39:04 +0100

    Add favourites properties.

--- src/app/favourite.service.ts
+++ src/app/favourite.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@angular/core';
+import {Recipe} from './recipe.service';
 
 @Injectable()
 export class FavouriteService {
+	private _favourites: Recipe[] = [];
+	private _storagename = 'FAVOURITES';
+	private _localStorage = window.localStorage;
 
-  constructor() {}
+
+	constructor() {}
 
 }
