3547f32ee0aee3789500d32290b620ca31eaef98
mswift42 <martin.haesler@gmail.com>
2016-07-09 17:09:04 +0100

    load favourites on init

--- src/app/favourites-view/favourites-view.component.ts
+++ src/app/favourites-view/favourites-view.component.ts
@@ -1,16 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { FavouriteService } from '../favourite.service';
+import { Recipe } from '../recipe.service';
+import {RecipeComponent} from '../recipe/recipe.component';
 
 @Component({
   moduleId: module.id,
   selector: 'app-favourites-view',
   templateUrl: 'favourites-view.component.html',
-  styleUrls: ['favourites-view.component.css']
+  styleUrls: ['favourites-view.component.css'],
+  directives: [RecipeComponent]
 })
 export class FavouritesViewComponent implements OnInit {
+  recipes: Recipe[];
 
-  constructor() {}
+  constructor(private _favouriteService: FavouriteService) {}
 
   ngOnInit() {
+  this._favouriteService.loadFromStorage();
+  this.recipes = this._favouriteService.favourites;
   }
 
 }
