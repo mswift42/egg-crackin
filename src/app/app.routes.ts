7ae697969fea99d6a95757d2388917866128cdab
mswift42 <martin.haesler@gmail.com>
2016-07-10 08:43:45 +0100

    Add file app.routes.ts

---
+++ src/app/app.routes.ts
@@ -0,0 +1,3 @@
+import {provideRouter, RouterConfig} from '@angular/router';
+import {RecipesviewComponent} from './recipesview-component/recipesview.component';
+import {FavouritesViewComponent} from './favourites-view/favourites-view.component';
