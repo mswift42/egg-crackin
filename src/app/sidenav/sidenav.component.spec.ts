0df235963ab142d3c47016e7ecd6e9e574c4de38
mswift42 <martin.haesler@gmail.com>
2016-07-11 13:13:45 +0100

    Generate Sidenav component

---
+++ src/app/sidenav/sidenav.component.spec.ts
@@ -0,0 +1,20 @@
+/* tslint:disable:no-unused-variable */
+
+import { By }           from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import {
+  beforeEach, beforeEachProviders,
+  describe, xdescribe,
+  expect, it, xit,
+  async, inject
+} from '@angular/core/testing';
+
+import { SidenavComponent } from './sidenav.component';
+
+describe('Component: Sidenav', () => {
+  it('should create an instance', () => {
+    let component = new SidenavComponent();
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/sidenav/sidenav.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-sidenav',
+  templateUrl: 'sidenav.component.html',
+  styleUrls: ['sidenav.component.css']
+})
+export class SidenavComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {
+  }
+
+}
