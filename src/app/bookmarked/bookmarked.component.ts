3cc63e51c3de9f15551647e32f5330bba747ba89
mswift42 <martin.haesler@gmail.com>
2016-07-12 13:14:11 +0100

    Add bookmarked inputs

--- src/app/bookmarked/bookmarked.component.ts
+++ src/app/bookmarked/bookmarked.component.ts
@@ -1,16 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input} from '@angular/core';
+import { FavouriteService } from '../favourite.service';
+import { RecipeImageComponent} from '../recipe-image/recipe-image.component';
+import { TitleComponent} from '../title/title.component';
+import { PublisherUrlComponent} from '../publisher-url/publisher-url.component';
+import {TooltipComponent} from '../tooltip/tooltip.component';
 @Component({
-  moduleId: module.id,
-  selector: 'app-bookmarked',
-  templateUrl: 'bookmarked.component.html',
-  styleUrls: ['bookmarked.component.css']
+    moduleId: module.id,
+    selector: 'app-bookmarked',
+    templateUrl: 'bookmarked.component.html',
+    styleUrls: ['bookmarked.component.css'],
+    directives: [TitleComponent, PublisherUrlComponent, 
+    RecipeImageComponent, TooltipComponent]
 })
-export class BookmarkedComponent implements OnInit {
+export class BookmarkedComponent {
+    @Input() title: string;
+    @Input() recipe_url: string;
+    @Input() image_url: string;
+    @Input() publisher: string;
+    @Input() publisher_url: string;
 
-  constructor() {}
+    constructor(private _favouriteService: FavouriteService) {}
 
-  ngOnInit() {
-  }
+    deleteFavourite(recipe_url: string) {
+        this._favouriteService.deleteFavourite(recipe_url);
+    }
+
+
+
 
 }
