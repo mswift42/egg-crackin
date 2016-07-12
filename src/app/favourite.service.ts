872f41977930a950027a8a7b4a990d06b3aec89f
mswift42 <martin.haesler@gmail.com>
2016-07-12 13:05:07 +0100

    Add deleteFavourite method

--- src/app/favourite.service.ts
+++ src/app/favourite.service.ts
@@ -3,32 +3,36 @@ import {Recipe} from './recipe.service';
 
 @Injectable()
 export class FavouriteService {
-	private _favourites: Recipe[] = [];
-	private _storagename = 'FAVOURITES';
-	private _localStorage = window.localStorage;
+    private _favourites: Recipe[] = [];
+    private _storagename = 'FAVOURITES';
+    private _localStorage = window.localStorage;
 
-	get favourites(): Recipe[] {
-		return this._favourites;
-	}
-
-
-	constructor() { }
-
-	saveFavourite(recipe: Recipe) {
-		this._favourites.unshift(recipe);
-		this.saveToStorage();
-	}
+    get favourites(): Recipe[] {
+        return this._favourites;
+    }
 
 
+    constructor() { }
 
-	saveToStorage() {
-		this._localStorage.setItem(this._storagename,
-			JSON.stringify(this._favourites));
-	}
+    saveFavourite(recipe: Recipe) {
+        this._favourites.unshift(recipe);
+        this.saveToStorage();
+    }
+    deleteFavourite(source_url: string) {
+        this._favourites = this._favourites.filter((i) => i.source_url !== source_url);
+        this.saveToStorage();
+    }
 
-	loadFromStorage() {
-		this._favourites =
-			JSON.parse(this._localStorage.getItem(this._storagename));
-	}
+
+
+    saveToStorage() {
+        this._localStorage.setItem(this._storagename,
+                                   JSON.stringify(this._favourites));
+    }
+
+    loadFromStorage() {
+        this._favourites =
+            JSON.parse(this._localStorage.getItem(this._storagename));
+    }
 
 }
