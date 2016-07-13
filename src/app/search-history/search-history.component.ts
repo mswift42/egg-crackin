6cd02a18d789ec5e35c315a77a5b150c3f92cbeb
mswift42 <martin.haesler@gmail.com>
2016-07-13 14:03:07 +0100

    Generate component search-history

---
+++ src/app/search-history/search-history.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-search-history',
+  templateUrl: 'search-history.component.html',
+  styleUrls: ['search-history.component.css']
+})
+export class SearchHistoryComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {
+  }
+
+}
