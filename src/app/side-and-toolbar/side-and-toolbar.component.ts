ff8176c5bbf7ea15fd68ffbd4b75c2ce8c9c7fd6
mswift42 <martin.haesler@gmail.com>
2016-07-16 13:03:16 +0100

    import and display search-history component

--- src/app/side-and-toolbar/side-and-toolbar.component.ts
+++ src/app/side-and-toolbar/side-and-toolbar.component.ts
@@ -1,22 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import {MdToolbar} from '@angular2-material/toolbar';
 import {MD_SIDENAV_DIRECTIVES} from '@angular2-material/sidenav';
 import {ROUTER_DIRECTIVES} from '@angular/router';
 import {MD_LIST_DIRECTIVES} from '@angular2-material/list';
 import {MD_BUTTON_DIRECTIVES} from '@angular2-material/button';
+import {SearchHistoryComponent} from '../search-history/search-history.component';
 @Component({
   moduleId: module.id,
   selector: 'app-side-and-toolbar',
   templateUrl: 'side-and-toolbar.component.html',
   styleUrls: ['side-and-toolbar.component.css'],
   directives: [ROUTER_DIRECTIVES, MdToolbar, MD_SIDENAV_DIRECTIVES,
-    MD_LIST_DIRECTIVES]
+    MD_LIST_DIRECTIVES, SearchHistoryComponent]
 })
-export class SideAndToolbarComponent implements OnInit {
+export class SideAndToolbarComponent {
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
