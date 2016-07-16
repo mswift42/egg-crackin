517b674ee894e16189c3b06df17faa00e4542f93
mswift42 <martin.haesler@gmail.com>
2016-07-16 13:28:52 +0100

    Save search in search history

--- src/app/recipesview-component/recipesview.component.ts
+++ src/app/recipesview-component/recipesview.component.ts
@@ -2,27 +2,31 @@ import { Component } from '@angular/core';
 import {Recipe, RecipeService} from '../recipe.service';
 import {RecipeComponent} from '../recipe/recipe.component';
 import {RecipeSearchComponent} from '../recipe-search/recipe-search.component';
+import {SearchHistoryService} from '../search-history.service';
+
 @Component({
-  moduleId: module.id,
-  selector: 'app-recipesview',
-  templateUrl: 'recipesview.component.html',
-  styleUrls: ['recipesview.component.css'],
-  directives: [RecipeComponent, RecipeSearchComponent]
+    moduleId: module.id,
+    selector: 'app-recipesview',
+    templateUrl: 'recipesview.component.html',
+    styleUrls: ['recipesview.component.css'],
+    directives: [RecipeComponent, RecipeSearchComponent]
 })
 export class RecipesviewComponent {
 
     recipes: Recipe[] = [];
 
-    constructor(private recipeservice: RecipeService) { }
+    constructor(private recipeservice: RecipeService,
+        private searchhistoryservice: SearchHistoryService) { }
 
     searchRecipes(query: string) {
+        this.searchhistoryservice.saveSearch(query);
         this.recipeservice.recipes = [];
         this.recipeservice.getRecipes(query)
             .subscribe((recs) => {
-                for (var i = 0;i<recs.length;i++) {
+                for (var i = 0; i < recs.length; i++) {
                     this.recipeservice.recipes.push(new Recipe(recs[i]["publisher"],
-                     this.recipeservice.unsantize(recs[i]["title"]), 
-                     recs[i]["source_url"], recs[i]["image_url"], recs[i]["publisher_url"]))
+                        this.recipeservice.unsantize(recs[i]["title"]),
+                        recs[i]["source_url"], recs[i]["image_url"], recs[i]["publisher_url"]))
                 }
                 this.recipes = this.recipeservice.recipes;
             });
