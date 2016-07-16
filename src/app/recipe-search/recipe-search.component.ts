e03e865eab82645352ac83d34f963c99bc16d81a
mswift42 <martin.haesler@gmail.com>
2016-07-16 11:10:33 +0100

    fix name for keyup.enter function

--- src/app/recipe-search/recipe-search.component.ts
+++ src/app/recipe-search/recipe-search.component.ts
@@ -1,18 +1,20 @@
 import { Component, Output, EventEmitter } from '@angular/core';
 import {SearchHistoryService} from '../search-history.service';
+import {MD_INPUT_DIRECTIVES} from '@angular2-material/input';
 
 @Component({
   moduleId: module.id,
   selector: 'app-recipe-search',
   templateUrl: 'recipe-search.component.html',
-  styleUrls: ['recipe-search.component.css']
+  styleUrls: ['recipe-search.component.css'],
+  directives: [MD_INPUT_DIRECTIVES]
 })
 export class RecipeSearchComponent {
-  @Output() onSubmit = new EventEmitter<string>();
+  @Output() onSearch = new EventEmitter<string>();
 
   constructor(private _searchHistoryService: SearchHistoryService) {}
 
   searchRecipe(query: string) {
-    this.onSubmit.emit(query);
+    this.onSearch.emit(query);
   }
 }
