818ed45434bf0049e818de51c6a9e4b35e4f26ec
mswift42 <martin.haesler@gmail.com>
2016-07-16 12:35:14 +0100

    Delete unneeded directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,6 @@ import { Component } from '@angular/core';
 import {RecipesviewComponent} from './recipesview-component/recipesview.component';
 import {RecipeService} from './recipe.service';
 import {FavouriteService} from './favourite.service';
-import {MdToolbar} from '@angular2-material/toolbar';
-import {MD_SIDENAV_DIRECTIVES} from '@angular2-material/sidenav';
-import {ROUTER_DIRECTIVES} from '@angular/router';
-import {MD_LIST_DIRECTIVES} from '@angular2-material/list';
-import {MD_BUTTON_DIRECTIVES} from '@angular2-material/button';
 import {SearchHistoryService} from './search-history.service';
 import {SideAndToolbarComponent} from './side-and-toolbar/side-and-toolbar.component';
 
@@ -15,9 +10,7 @@ import {SideAndToolbarComponent} from './side-and-toolbar/side-and-toolbar.compo
     selector: 'app-root',
     templateUrl: 'app.component.html',
     styleUrls: ['app.component.css'],
-    directives: [RecipesviewComponent, MdToolbar, ROUTER_DIRECTIVES,
-                 MD_SIDENAV_DIRECTIVES, MD_LIST_DIRECTIVES,
-                 MD_BUTTON_DIRECTIVES, SideAndToolbarComponent],
+    directives: [SideAndToolbarComponent],
     providers: [RecipeService, FavouriteService, SearchHistoryService]
 })
 export class AppComponent {
