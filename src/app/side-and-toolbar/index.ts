2184b481014d39854818ab2c7334e6fc35ac2234
mswift42 <martin.haesler@gmail.com>
2016-07-16 12:00:41 +0100

    Generate component side-and-toolbar

---
+++ src/app/side-and-toolbar/index.ts
@@ -0,0 +1,1 @@
+export * from './side-and-toolbar.component';
---
+++ src/app/side-and-toolbar/side-and-toolbar.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-side-and-toolbar',
+  templateUrl: 'side-and-toolbar.component.html',
+  styleUrls: ['side-and-toolbar.component.css']
+})
+export class SideAndToolbarComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {
+  }
+
+}
