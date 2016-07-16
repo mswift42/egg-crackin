e6720f651df9c82f72ff248f15a456378c56015b
mswift42 <martin.haesler@gmail.com>
2016-07-16 13:28:33 +0100

    initialize searchhistory from storage correctly

--- src/app/search-history.service.ts
+++ src/app/search-history.service.ts
@@ -2,22 +2,25 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class SearchHistoryService {
-  private _searchhistory = [];
+  private _searchhistory: string[] = [];
   private _storagename = 'SEARCHHISTORY';
   private _localStorage = window.localStorage;
 
-  constructor() {}
+  constructor() { }
 
   contains(search: string): boolean {
-    return this._searchhistory.filter((i) => (i === search)).length > 0;
+    return this.searchhistory.filter((i) => (i === search)).length > 0;
   }
   get searchhistory(): string[] {
     return this._searchhistory;
   }
+  addToHistory(query: string) {
+    this._searchhistory.unshift(query);
+  }
 
   saveSearch(search: string) {
     if (!(this.contains(search))) {
-      this._searchhistory.unshift(search);
+      this.addToHistory(search);
       this.saveToStorage();
       if (this._searchhistory.length > 10) {
         this._searchhistory.pop();
@@ -31,7 +34,7 @@ export class SearchHistoryService {
   }
 
   loadFromStorage() {
-    this._searchhistory = JSON.parse(this._localStorage.getItem(this._storagename));
-    }
+    this._searchhistory = JSON.parse(this._localStorage.getItem(this._storagename)) || [];
+  }
 
 }
