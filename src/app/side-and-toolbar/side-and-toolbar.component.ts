8077f51bbe3fbe6a11bb5503d0506a44bc023cd6
mswift42 <martin.haesler@gmail.com>
2016-07-17 09:26:18 +0100

    conditionally set sidebar to opened

--- src/app/side-and-toolbar/side-and-toolbar.component.ts
+++ src/app/side-and-toolbar/side-and-toolbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit , ElementRef} from '@angular/core';
 import {MdToolbar} from '@angular2-material/toolbar';
 import {MD_SIDENAV_DIRECTIVES} from '@angular2-material/sidenav';
 import {ROUTER_DIRECTIVES} from '@angular/router';
@@ -16,8 +16,15 @@ import {FavouritesViewComponent} from '../favourites-view/favourites-view.compon
     MD_LIST_DIRECTIVES, SearchHistoryComponent],
   precompile: [RecipesviewComponent, FavouritesViewComponent]
 })
-export class SideAndToolbarComponent {
+export class SideAndToolbarComponent  implements OnInit{
+  isOponed: boolean = false;
 
-  constructor() { }
+  constructor(private _elementRef: ElementRef) { }
+
+  ngOnInit() {
+    if (window.screen.width > 800) {
+      this.isOponed = true;
+    }
+  }
 
 }
