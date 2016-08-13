b19e1e1ea28cbf486c459d487b944ce15aa1ae6c
mswift42 <martin.haesler@gmail.com>
2016-08-13 09:54:28 +0100

    Update to rc.5

--- src/app/recipe.service.ts
+++ src/app/recipe.service.ts
@@ -21,7 +21,6 @@ export class RecipeService {
     get food2forkKey(): string {
         return this._FOOD2FORKAPIKEY;
     }
-    
     queryUrl(query: string): string {
         let split = query.trim().split(" ");
         let url = "/searchrecipe?query=";
